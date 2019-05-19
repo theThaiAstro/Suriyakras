457a0b7a12e27f8393c101560758aa5200ef2b51
Thammarith <thammarith@users.noreply.github.com>
2019-05-19 13:12:53 +0700

    Add $CATEGORY_IDS

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
   /* Global variables */
   $THAI_MONTHS = ["มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"];
+  $CATEGORY_IDS = array( "featured" => 4, "article" => 5 );
   /* End of global variables */
 
   /* Global fundtions */
