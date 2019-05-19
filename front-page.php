d72e8400c7082b943196f756229e97fa0cd950bc
Thammarith <thammarith@users.noreply.github.com>
2019-05-19 14:45:33 +0700

    Use get_template_part with events

--- front-page.php
+++ front-page.php
@@ -4,46 +4,12 @@
   <!-- For outlining and semantic purpose -->
   <h1 style="display: none;">หน้าหลัก - homepage</h1>
 
+  <!-- Featured article -->
   <?php get_template_part('templates/featured-article'); ?>
-
+  
   <!-- Recent articles -->
-  <section id="recent-articles">
-    <h2 class="section-title">บทความล่าสุด</h2>
-
-    <a class="card-link" href="#">
-      <div class="card-container">
-        <img alt="{{image alt}}" class="card-image-featured" src="./assets/img/featured-background.jpg" />
-        <div class="card-text">
-          <div class="category-badge">หมวดหมู่</div>
-          <h3 class="card-title">นาซาก็พาเธอกลับมาไม่ได้</h3>
-          <div class="card-time">29 กุมภาพันธ์ 2563</div>
-        </div>
-      </div>
-    </a>
-
-    <a class="card-link" href="#">
-      <div class="card-container regular">
-        <img alt="{{image alt}}" class="card-image-regular" src="./assets/img/featured-background.jpg" />
-        <div class="card-text">
-          <div class="category-badge small">หมวดหมู่</div>
-          <h3 class="card-title">นาซาก็พาเธอกลับมาไม่ได้</h3>
-          <div class="card-time regular">29 กุมภาพันธ์ 2562</div>
-        </div>
-      </div>
-    </a>
-
-    <a class="card-link" href="#">
-      <div class="card-container regular">
-        <img alt="{{image alt}}" class="card-image-regular" src="./assets/img/featured-background.jpg" />
-        <div class="card-text">
-          <div class="category-badge small">หมวดหมู่</div>
-          <h3 class="card-title">นาซาก็พาเธอกลับมาไม่ได้ นาซาก็พาเธอกลับมาไม่ได้ นาซาก็พาเธอกลับมาไม่ได้</h3>
-          <div class="card-time regular">29 กุมภาพันธ์ 2563</div>
-        </div>
-      </div>
-    </a>
-  </section>
-
+  <?php get_template_part('templates/articles'); ?>
+  
   <!-- Events -->
   <?php get_template_part('templates/events'); ?>
   
