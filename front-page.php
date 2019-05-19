53c207a1c374ea4a6d095131f9f4e1cd58154fb6
Thammarith <thammarith@users.noreply.github.com>
2019-05-19 15:03:04 +0700

    Use template part for phenomena

--- front-page.php
+++ front-page.php
@@ -12,46 +12,10 @@
   
   <!-- Events -->
   <?php get_template_part('templates/events'); ?>
-  
 
   <!-- Phenomena -->
-  <section id="sky-phenomena">
-    <h2 class="section-title">ปรากฏการณ์ท้องฟ้า</h2>
-
-    <a class="card-link" href="#">
-      <div class="card-container">
-        <img alt="{{image alt}}" class="card-image-featured" src="./assets/img/featured-background.jpg" />
-        <div class="card-text">
-          <div class="category-badge">หมวดหมู่</div>
-          <h3 class="card-title">นาซาก็พาเธอกลับมาไม่ได้</h3>
-          <div class="card-time">29 กุมภาพันธ์ 2563</div>
-        </div>
-      </div>
-    </a>
-
-    <a class="card-link" href="#">
-      <div class="card-container regular">
-        <img alt="{{image alt}}" class="card-image-regular" src="./assets/img/featured-background.jpg" />
-        <div class="card-text">
-          <div class="category-badge small">หมวดหมู่</div>
-          <h3 class="card-title">นาซาก็พาเธอกลับมาไม่ได้</h3>
-          <div class="card-time regular">29 กุมภาพันธ์ 2563</div>
-        </div>
-      </div>
-    </a>
-
-    <a class="card-link" href="#">
-      <div class="card-container regular">
-        <img alt="{{image alt}}" class="card-image-regular" src="./assets/img/featured-background.jpg" />
-        <div class="card-text">
-          <div class="category-badge small">หมวดหมู่</div>
-          <h3 class="card-title">นาซาก็พาเธอกลับมาไม่ได้</h3>
-          <div class="card-time regular">29 กุมภาพันธ์ 2563</div>
-        </div>
-      </div>
-    </a>
-  </section>
-
+  <?php get_template_part('templates/phenomena'); ?>
+  
   <!-- Gallery -->
   <section id="gallery">
     <h2 class="section-title">ห้องภาพท้องฟ้า</h2>
