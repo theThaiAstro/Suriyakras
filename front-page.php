824e5a83a079358d6d4a8811eb0aa22fc93846b1
Thammarith <thammarith@users.noreply.github.com>
2019-05-19 12:48:18 +0700

    Add sticky post

--- front-page.php
+++ front-page.php
@@ -4,14 +4,38 @@
   <!-- For outlining and semantic purpose -->
   <h1 style="display: none;">หน้าหลัก - homepage</h1>
 
-  <section id="featured-story">
-    <div id="featured-story-darken-overlay">
-      <div id="featured-story-heading">
-        <div class="category-badge">หมวดหมู่</div>
-        <h2>ทดสอบ พาดหัวภาษาไทย ไหนลองยาว ๆ ดู</h2>
-      </div>
-    </div>
-  </section>
+  <?php
+    $stickies = get_option( 'sticky_posts' );
+    if ( $stickies ) :
+
+      $the_query = new WP_Query([
+        'post_type'           => 'post',
+        'post__in'            => $stickies,
+        'posts_per_page'      => 1,
+        'ignore_sticky_posts' => 1
+      ]);
+
+      if ( $the_query->have_posts() ) :
+        while ( $the_query->have_posts() ) :
+          $the_query->the_post();
+  ?>
+          <section id="featured-story">
+            <div id="featured-story-darken-overlay">
+              <div id="featured-story-heading">
+                <?php foreach(get_the_category() as $category) : ?>
+                  <div class="category-badge"><?php echo $category->name; ?></div>
+                <?php endforeach; ?>
+                <h2><?php the_title() ;?></h2>
+              </div>
+            </div>
+          </section>
+
+  <?php
+        endwhile;
+        wp_reset_postdata();
+      endif;
+    endif;
+  ?>
 
   <!-- Recent articles -->
   <section id="recent-articles">
