8d3dcc9e5c5d5b29a9048248aad22b30efba2998
Thammarith <thammarith@users.noreply.github.com>
2019-05-21 23:37:25 +0700

    Use the new card layout

--- templates/events.php
+++ templates/events.php
@@ -1,60 +1,66 @@
-<section id="events">
-  <h2 class="section-title">กิจกรรม</h2>
+<section id="events" class="card-section">
 
-  <?php
-    $CATEGORY_IDS = get_CATEGORY_IDS();
+  <div class="section-title-container">
+    <h2 class="section-title">กิจกรรม</h2>
+  </div>
 
-    $the_query = new WP_Query([
-      'post_type'           => 'post',
-      'category__in'        => array($CATEGORY_IDS['events'], $CATEGORY_IDS['free-events']), // NEWS too?
-      'post__not_in'        => get_option( 'sticky_posts' ), // ignores sticky post
-      'posts_per_page'      => 4,
-      'ignore_sticky_posts' => 1
-    ]);
+  <div class="card-container">
+    <?php
+      $CATEGORY_IDS = get_CATEGORY_IDS();
 
-    if ( $the_query->have_posts() ) :
+      $the_query = new WP_Query([
+        'post_type'           => 'post',
+        'category__in'        => array($CATEGORY_IDS['events'], $CATEGORY_IDS['free-events']), // NEWS too?
+        'post__not_in'        => get_option( 'sticky_posts' ), // ignores sticky post
+        'posts_per_page'      => 4,
+        'ignore_sticky_posts' => 1
+      ]);
 
-      $order = 0;
-      while ( $the_query->have_posts() ) :
-        $is_first = $order === 0;
-        $the_query->the_post();
-        ?>
+      if ( $the_query->have_posts() ) :
 
-        <a class="card-link" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-          <div class="card-container <?php if ( !$is_first ) echo 'regular'; ?>">
-            <img
-              alt="{{image alt}}"
-              class="card-image-<?php echo $is_first ? 'featured' : 'regular' ?>"
-              src="<?php
-                if (has_post_thumbnail()) the_post_thumbnail_url();
-                else echo get_template_directory_uri() . '/assets/img/default-thumbnail.png'
-              ?>" />
-            <div class="card-text">
-              <?php foreach(get_the_category() as $category) : ?>
-                <div 
-                  class="category-badge
-                    <?php if ( !$is_first ) echo 'small'; ?>
-                    <?php if ( $category->cat_ID === $CATEGORY_IDS['free-events'] ) echo 'red bold'; ?>
-                  "><?php echo $category->name; ?></div>
-              <?php endforeach; ?>
+        $order = 0;
+        while ( $the_query->have_posts() ) :
+          $is_first = $order === 0;
+          $the_query->the_post();
+          ?>
 
-              <h3 class="card-title"><?php the_title(); ?></h3>
-              <div class="card-time <?php if ( !$is_first ) echo 'regular'; ?>">
-                <?php the_time('j'); ?> <?php echo get_THAI_MONTHS()[get_the_time('n')]; ?> <?php echo CE2BE(get_the_time('Y')); ?> · <?php the_time('H:i'); ?>
+          <a class="card-link" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+            <div class="card <?php if ( $is_first ) echo 'featured'; ?>">
+              <div
+                class="card-image"
+                style="background-image: url('<?php
+                  if (has_post_thumbnail()) the_post_thumbnail_url();
+                  else echo get_template_directory_uri() . '/assets/img/default-thumbnail.png'
+                ?>');"
+              ></div>
+
+              <div class="card-content">
+                <?php foreach(get_the_category() as $category) : ?>
+                  <div 
+                    class="category-badge
+                      <?php if ( !$is_first ) echo 'small'; ?>
+                      <?php if ( $category->cat_ID === $CATEGORY_IDS['free-events'] ) echo 'red bold'; ?>
+                    "><?php echo $category->name; ?></div>
+                <?php endforeach; ?>
+
+                <h3 class="card-title"><?php the_title(); ?></h3>
+                <div class="card-time <?php if ( !$is_first ) echo 'regular'; ?>">
+                  <?php the_time('j'); ?> <?php echo get_THAI_MONTHS()[get_the_time('n')]; ?> <?php echo CE2BE(get_the_time('Y')); ?> · <?php the_time('H:i'); ?>
+                </div>
               </div>
             </div>
-          </div>
-        </a>
+          </a>
 
-      <?php
-        $order++;
-      endwhile;
-      wp_reset_postdata();
-    else:
-  ?>
-    <!-- TODO: STYLE -->
-    ยังไม่มีกิจกรรม 
-  <?php
-    endif;
-  ?>
+        <?php
+          $order++;
+        endwhile;
+        wp_reset_postdata();
+      else:
+    ?>
+      <!-- TODO: STYLE -->
+      ยังไม่มีกิจกรรม 
+    <?php
+      endif;
+    ?>
+  </div>
 </section>
--- templates/gallery.php
+++ templates/gallery.php
@@ -1,56 +1,61 @@
-<section id="gallery">
-  <h2 class="section-title">ห้องภาพท้องฟ้า</h2>
+<section id="gallery" class="card-section">
 
-  <?php
-    $CATEGORY_IDS = get_CATEGORY_IDS();
+  <div class="section-title-container">
+    <h2 class="section-title">ห้องภาพท้องฟ้า</h2>
+  </div>
 
-    $the_query = new WP_Query([
-      'post_type'           => 'post',
-      'category__in'        => array($CATEGORY_IDS['gallery']), // NEWS too?
-      'post__not_in'        => get_option( 'sticky_posts' ), // ignores sticky post
-      'posts_per_page'      => 4,
-      'ignore_sticky_posts' => 1
-    ]);
+  <div class="cards-container">
+    <?php
+      $CATEGORY_IDS = get_CATEGORY_IDS();
 
-    if ( $the_query->have_posts() ) :
+      $the_query = new WP_Query([
+        'post_type'           => 'post',
+        'category__in'        => array($CATEGORY_IDS['gallery']),
+        'post__not_in'        => get_option( 'sticky_posts' ), // ignores sticky post
+        'posts_per_page'      => 4,
+        'ignore_sticky_posts' => 1
+      ]);
 
-      $order = 0;
-      while ( $the_query->have_posts() ) :
-        $is_first = $order === 0;
-        $the_query->the_post();
-        ?>
+      if ( $the_query->have_posts() ) :
 
-        <a class="card-link" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-          <div class="card-container <?php if ( !$is_first ) echo 'regular'; ?>">
-            <img
-              alt="{{image alt}}"
-              class="card-image-<?php echo $is_first ? 'featured' : 'regular' ?>"
-              src="<?php
-                if (has_post_thumbnail()) the_post_thumbnail_url();
-                else echo get_template_directory_uri() . '/assets/img/default-thumbnail.png'
-              ?>" />
-            <div class="card-text">
+        $order = 0;
+        while ( $the_query->have_posts() ) :
+          $is_first = $order === 0;
+          $the_query->the_post();
+          ?>
 
-              <?php foreach(get_the_category() as $category) : ?>
-                <div class="category-badge <?php if ( !$is_first ) echo 'small'; ?>"><?php echo $category->name; ?></div>
-              <?php endforeach; ?>
+          <a class="card-link" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+            <div class="card <?php if ( $is_first ) echo 'featured'; ?>">
+              <div
+                class="card-image"
+                style="background-image: url('<?php
+                  if (has_post_thumbnail()) the_post_thumbnail_url();
+                  else echo get_template_directory_uri() . '/assets/img/default-thumbnail.png'
+                ?>');"
+              ></div>
 
-              <h3 class="card-title"><?php the_title(); ?></h3>
-              <div class="card-time <?php if ( !$is_first ) echo 'regular'; ?>">
-                <?php the_time('j'); ?> <?php echo get_THAI_MONTHS()[get_the_time('n')]; ?> <?php echo CE2BE(get_the_time('Y')); ?> · <?php the_time('H:i'); ?>
+              <div class="card-content">
+                <?php foreach(get_the_category() as $category) : ?>
+                  <div class="category-badge <?php if ( !$is_first ) echo 'small'; ?>"><?php echo $category->name; ?></div>
+                <?php endforeach; ?>
+
+                <h3 class="card-title"><?php the_title(); ?></h3>
+                <div class="card-time <?php if ( !$is_first ) echo 'regular'; ?>">
+                  <?php the_time('j'); ?> <?php echo get_THAI_MONTHS()[get_the_time('n')]; ?> <?php echo CE2BE(get_the_time('Y')); ?> · <?php the_time('H:i'); ?>
+                </div>
               </div>
             </div>
-          </div>
-        </a>
+          </a>
 
-      <?php
-        $order++;
-      endwhile;
-      wp_reset_postdata();
-    else:
-  ?>
-    ยังไม่มีข้อมูล
-  <?php
-    endif;
-  ?>
+        <?php
+          $order++;
+        endwhile;
+        wp_reset_postdata();
+      else:
+    ?>
+      ยังไม่มีข้อมูล
+    <?php
+      endif;
+    ?>
+  </div>
 </section>
