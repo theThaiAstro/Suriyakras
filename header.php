f6d55870d828a051c6ada7f6a02c2a00d5fb9533
Thammarith <thammarith@users.noreply.github.com>
2019-06-01 15:46:42 +0700

    Add icons

--- header.php
+++ header.php
@@ -2,9 +2,14 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="<?php bloginfo('description'); ?>">
+  <meta name="description" content="<?php bloginfo('description'); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/favicon-16x16.png">
+  <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/site.webmanifest">
 
   <title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' |'; } ?> <?php bloginfo('name'); ?></title>
   <?php wp_head(); ?>
