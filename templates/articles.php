550a62f2dc3ade55d5464b8188a1b72276790344
Thammarith <thammarith@users.noreply.github.com>
2019-06-01 15:01:17 +0700

    Update front-page background image for articles

--- templates/articles.php
+++ templates/articles.php
@@ -1,8 +1,8 @@
 <section id="articles" class="card-section">
 
   <div class="section-title-container">
-    <h2 class="section-title">
-      <a class="section-title-link" href="<?php echo get_category_link(get_CATEGORY_IDS()['articles']); ?>">บทความล่าสุด</a>
+    <h2 class="section-title white">
+      <a class="section-title-link" href="<?php echo get_category_link(get_CATEGORY_IDS()['articles']); ?>">บทความ</a>
     </h2>
   </div>
 
